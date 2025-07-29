612d06f6d519590f2c54db510a3f7a39b30b24a6
Kamich-GP <yurochkagameplus@gmail.com>
2025-07-29 21:13:05 +0500

    urls, views

--- index/views.py
+++ index/views.py
@@ -3,6 +3,7 @@ from .models import Category, Product
 
 
 # Create your views here.
+# Главная страница
 def home_page(request):
     products = Product.objects.all()
     categories = Category.objects.all()
@@ -11,3 +12,23 @@ def home_page(request):
         'categories': categories
     }
     return render(request, 'home.html', context)
+
+
+# Страница с товарами по категории
+def category_page(request, pk):
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
+    context = {
+        'category': category,
+        'products': products
+    }
+    return render(request, 'category.html', context)
+
+
+# Страница с определенным товаром
+def product_page(request, pk):
+    product = Product.objects.get(id=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'product.html', context)
